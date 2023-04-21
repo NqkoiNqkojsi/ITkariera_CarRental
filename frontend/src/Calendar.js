5d3b34687718799d2442387b2f1859357672bd58
Stelian Grozev <tanya_dzh@abv.bg>
2023-04-22 01:16:13 +0300

    calendar with a bit of errors

--- frontend/src/Calendar.js
+++ frontend/src/Calendar.js
@@ -1,22 +1,88 @@
 import * as React from 'react';
+import {memo} from 'react';
+import PropTypes from 'prop-types';
 import dayjs from 'dayjs';
-import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+import isBetweenPlugin from 'dayjs/plugin/isBetween';
+import { styled } from '@mui/material/styles';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { PickersDay } from '@mui/x-date-pickers/PickersDay';
 
-class CustomDateCalendar extends React.Component{
-  render(){
-    return (
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DemoContainer components={['DateCalendar', 'DateCalendar']}>
-          <DemoItem>
-            <DateCalendar defaultValue={dayjs()} readOnly />
-          </DemoItem>
-        </DemoContainer>
-      </LocalizationProvider>
-    );
+dayjs.extend(isBetweenPlugin);
+
+const CustomPickersDay = styled(PickersDay, {
+  shouldForwardProp: (prop) =>
+    prop !== 'dayIsBetween' && prop !== 'isFirstDay' && prop !== 'isLastDay',
+})(({ theme, dayIsBetween, isFirstDay, isLastDay }) => ({
+  ...(dayIsBetween && {
+    borderRadius: 0,
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white,
+    '&:hover, &:focus': {
+      backgroundColor: theme.palette.primary.dark,
+    },
+  }),
+  ...(isFirstDay && {
+    borderTopLeftRadius: '50%',
+    borderBottomLeftRadius: '50%',
+  }),
+  ...(isLastDay && {
+    borderTopRightRadius: '50%',
+    borderBottomRightRadius: '50%',
+  }),
+}));
+
+function Day(props) {
+  const { day, selectedDay1, selectedDay2, ...other } = props;
+
+  if (selectedDay1 == null || selectedDay2 == null) {
+    return <PickersDay day={day} {...other} />;
   }
+
+  const start = selectedDay1;
+  const end = selectedDay2;
+
+  const dayIsBetween = day.isBetween(start, end, null, '[]');
+  const isFirstDay = day.isSame(start, 'day');
+  const isLastDay = day.isSame(end, 'day');
+
+  return (
+    <CustomPickersDay
+      {...other}
+      day={day}
+      sx={dayIsBetween ? { px: 2.5, mx: 0 } : {}}
+      dayIsBetween={dayIsBetween}
+      isFirstDay={isFirstDay}
+      isLastDay={isLastDay}
+    />
+  );
 }
 
-export default CustomDateCalendar;
+Day.propTypes = {
+  /**
+   * The date to show.
+   */
+  day: PropTypes.object.isRequired,
+  selectedDay1: PropTypes.object,
+  selectedDay2: PropTypes.object,
+};
+
+function CustomDay({value1, value2}) {
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar
+        readOnly={true}
+        slots={{ day: Day }}
+        slotProps={{
+          day: {
+            selectedDay1: value1,
+            selectedDay2: value2,
+          },
+        }}
+      />
+    </LocalizationProvider>
+  );
+}
+export default memo(CustomDay);
