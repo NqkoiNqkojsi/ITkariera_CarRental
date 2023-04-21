3f78a27856d9d33c84d56f6b96c55aada00311e9
Stelian Grozev <47190564+NqkoiNqkojsi@users.noreply.github.com>
2023-04-21 10:29:07 +0300

    Update Selector.js

--- frontend/src/Selector.js
+++ frontend/src/Selector.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
 import './App.css';
-import CustomDay from './Calendar'
+import CustomDateCalendar from './Calendar'
+import CarCard from './CarCard'
 import { DateField } from '@mui/x-date-pickers/DateField';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
@@ -12,45 +14,80 @@ import dayjs from 'dayjs';
 class SelectionMenu extends React.Component{
     constructor(props) {
         super(props);
-        this.state = { value: dayjs('2022-04-17')};
-        this.handleChange = this.handleChange.bind(this);
+        this.state = { value1: dayjs(), value2: dayjs(), daysCount:1};
+        this.handleChange1 = this.handleChange1.bind(this);
+        this.handleChange2 = this.handleChange2.bind(this);
     }
 
     render(){
         return (
             <Grid container spacing={2} columns={10}>
-                <Grid item xs={4}>
+                <Grid item xs={3}>
                     <Grid container spacing={2} columns={1}>
                         <LocalizationProvider dateAdapter={AdapterDayjs}>
-                            <Grid item xs={1} sx={{marginTop: 4, marginLeft:3, marginRight:0 }}>
+                            <Grid item xs={1} sx={{marginTop: 5, marginLeft:4, marginRight:0 }}>
                                 <DateField
                                     label="Start Date"
-                                    value={this.state.value}
-                                    onChange={this.handleChange}
+                                    value={this.state.value1}
+                                    onChange={this.handleChange1}
                                     format="DD-MM-YYYY"
+                                    disablePast={true}
+                                    maxDate={dayjs("12/12/2023")}
                                 />
                             </Grid>
-                            <Grid item xs={1} sx={{marginTop: 2, marginLeft:3, marginRight:0 }}>
+                            <Grid item xs={1} sx={{marginTop: 2, marginLeft:4, marginRight:0 }}>
                                 <DateField
                                     label="End Date"
-                                    value={this.state.value}
-                                    onChange={this.handleChange}
+                                    value={this.state.value2}
+                                    onChange={this.handleChange2}
                                     format="DD-MM-YYYY"
+                                    disablePast={true}
+                                    maxDate={dayjs("12/12/2023")}
                                 />
                             </Grid>
                         </LocalizationProvider>
+                        <Grid item xs={1} sx={{marginTop: 2, marginLeft:4, marginRight:0 }}>
+                            <Typography variant="subtitle1" component="div">
+                                Sum Days:{this.state.daysCount}
+                            </Typography>
+                        </Grid>
                     </Grid>
                 </Grid>
-                <Grid item xs={6} sx={{marginTop: 0, marginLeft:0 }}>
-                    <CustomDay/>
+                <Grid item xs={7} sx={{marginTop: 2, marginLeft:0 }}>
+                    <CustomDateCalendar/>
                 </Grid>
                 <Divider></Divider>
+                <Grid item xs={10}>
+                    <CarCard></CarCard>
+                </Grid>
+                <Grid item xs={10}>
+                    <CarCard></CarCard>
+                </Grid>
+                <Grid item xs={10}>
+                    <CarCard></CarCard>
+                </Grid>
             </Grid>  
         );
     }
 
-    handleChange(e) {
-        this.setState({ value: e.target.value });
+    handleChange1(e) {
+        this.setState({ daysCount:Math.abs(e.diff(this.state.value2, 'days'))});
+        this.setState({ value1: e.toDate() });
+        if(e.isAfter(this.state.value2)){
+            
+        }else{
+
+        }
+    }
+
+    handleChange2(e) {
+        this.setState({ daysCount:Math.abs(e.diff(this.state.value1, 'days'))});
+        this.setState({ value2: e.toDate() });
+        if(e.isBefore(this.state.value1)){
+
+        }else{
+            
+        }
     }
 }
 
