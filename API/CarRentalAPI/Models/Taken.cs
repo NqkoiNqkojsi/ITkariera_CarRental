6eb5e07febd357dbedeb891d24e0fc35c398f712
stoyanKucarov <devorer@abv.bg>
2023-04-21 17:20:31 +0300

    API Initial

---
+++ API/CarRentalAPI/Models/Taken.cs
@@ -0,0 +1,10 @@
+﻿namespace CarRentalAPI.Models
+{
+    public class Taken
+    {
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+
+        public List<Car> Cars { get; set; }
+    }
+}
