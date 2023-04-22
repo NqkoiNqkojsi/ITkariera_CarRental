201de7b138d8b63c5d26dd44e4d1bcc7bc8aa798
Stelian Grozev <tanya_dzh@abv.bg>
2023-04-22 16:05:19 +0300

    worked on carInfo

--- frontend/src/CarInfo.js
+++ frontend/src/CarInfo.js
@@ -4,6 +4,7 @@ import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
 import './Animations.scss';
+import './Info.css';
 import CustomDay from './Calendar'
 import CarCard from './CarCard'
 import { DateField } from '@mui/x-date-pickers/DateField';
@@ -14,57 +15,37 @@ import dayjs from 'dayjs';
 class InfoMenu extends React.Component{
     constructor(props) {
         super(props);
-        this.state = { value1: dayjs(), value2: dayjs(), daysCount:1, isSelectedCar:false, carId:0};
-        this.handleChange1 = this.handleChange1.bind(this);
-        this.handleChange2 = this.handleChange2.bind(this);
+        this.state = { brand: "Toyota", model:"Supra Mk4", type:"Sports Car", year:"1999", space:"5",price: 52.99, img:"/models/toyota_supra_mk4__supra_1997__free_download/thumbnail.jpg"};
+
     }
 
     render(){
         return (
-            <Grid container spacing={2} columns={10}>
-                <Grid item xs={3}>
-                    <Grid container spacing={2} columns={1}>
-                        <LocalizationProvider dateAdapter={AdapterDayjs}>
-                            <Grid item xs={1} sx={{marginTop: 5, marginLeft:4, marginRight:0 }}>
-                                <DateField
-                                    label="Start Date"
-                                    value={this.state.value1}
-                                    onChange={this.handleChange1}
-                                    format="DD-MM-YYYY"
-                                    disablePast={true}
-                                    maxDate={dayjs("12/12/2023")}
-                                />
-                            </Grid>
-                            <Grid item xs={1} sx={{marginTop: 2, marginLeft:4, marginRight:0 }}>
-                                <DateField
-                                    label="End Date"
-                                    value={this.state.value2}
-                                    onChange={this.handleChange2}
-                                    format="DD-MM-YYYY"
-                                    disablePast={true}
-                                    maxDate={dayjs("12/12/2023")}
-                                />
-                            </Grid>
-                        </LocalizationProvider>
-                        <Grid item xs={1} sx={{marginTop: 2, marginLeft:4, marginRight:0 }}>
-                            <Typography variant="subtitle1" component="div">
-                                Sum Days:{this.state.daysCount}
-                            </Typography>
+            <figure className="product-card">
+                <div className='containerImg'>
+                    <img className="product-card-img" src={this.state.img} alt={this.state.model}/>
+                    <button onClick={()=>this.props.close()} className="previous round">&#8249;</button>
+                </div>
+                <figcaption>
+                    <h1 className="product-card-title">{this.state.brand} {this.state.model}</h1>
+                    <Grid container spacing={2} columns={10}>
+                        <Grid item xs={5}>
+                            <h3>Year: {this.state.year}</h3>
+                            <h3 className="product-card-address">
+                                Space: {this.state.space}
+                            </h3>
+                            <button className="ghost glow" id="Rent">Rent</button> 
+                        </Grid>
+                        <Grid item xs={5}>
+                            <p className="original-price">${this.state.price}/day</p>
+                            <p className="amount-price">
+                            <span className="from">all in:</span>
+                            ${this.state.price*this.props.days}<span className="from"></span>
+                            </p>
                         </Grid>
                     </Grid>
-                </Grid>
-                <Grid item xs={7} sx={{marginTop: 2, marginLeft:0 }}>
-                    <CustomDay value1={this.state.value1} value2={this.state.value2}/>
-                </Grid>
-                <Divider orientation="horizontal" variant="middle"/>
-                {[0,0,0,0,0].map(() => 
-                    <Grid item xs={10}>
-                        <div className='glow cardCar'>
-                        <CarCard></CarCard>
-                        </div>
-                    </Grid>)
-                }
-            </Grid>  
+                </figcaption>
+            </figure>
         );
     }
 
